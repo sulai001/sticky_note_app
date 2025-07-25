148f0829fef95e469083576501b336327900a69f
sulai001 <sulaiirf2930@gmail.com>
2025-07-26 00:22:06 +0530

    asdsd

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
